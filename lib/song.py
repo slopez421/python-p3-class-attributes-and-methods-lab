1df34e40c3d569bc01f1544485b1a0e73b1a527e
Shania Lopez <slopez@fandm.edu>
2023-08-29 20:08:34 -0400

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,44 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.genre = genre
+        self.artist = artist
+        self.add_to_song_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre in cls.genres:
+            return None
+        else:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.append(artist)
+
+    @classmethod
+    def add_to_song_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
